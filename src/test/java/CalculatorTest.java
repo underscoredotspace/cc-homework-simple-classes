26c48bbcede96fe3f988d5d50b68763fa0dbc436
Colin Tindle <ampersand@underscore.space>
2018-05-14 18:10:48 +0100

    initial commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,16 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+  private Calculator calculator;
+
+  @Before
+  private void before() {
+    calculator = new Calculator();
+  }
+
+  @Test
+  private void addTwoNumbers() {
+
+  }
+}
