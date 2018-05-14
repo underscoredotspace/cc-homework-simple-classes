62030ae011dc0a635e791238ecb9643949949aad
Colin Tindle <ampersand@underscore.space>
2018-05-15 00:37:05 +0100

    Added WaterBottle class

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -1,2 +1,23 @@
 public class WaterBottle {
+  private int volume;
+
+  WaterBottle() {
+    this.fill();
+  }
+
+  public int getVolume() {
+    return this.volume;
+  }
+
+  public void drink() {
+    this.volume -= 10;
+  }
+
+  public void empty() {
+    this.volume = 0;
+  }
+
+  public void fill() {
+    this.volume = 100;
+  }
 }
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,37 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+  private WaterBottle waterBottle;
+
+  @Before
+  public void before() {
+    waterBottle = new WaterBottle();
+  }
+
+  @Test
+  public void start() {
+    assertEquals(100, waterBottle.getVolume());
+  }
+
+  @Test
+  public void drink() {
+    waterBottle.drink();
+    assertEquals(90, waterBottle.getVolume());
+  }
+
+  @Test
+  public void empty() {
+    waterBottle.empty();
+    assertEquals(0, waterBottle.getVolume());
+  }
+
+  @Test
+  public void fill() {
+    waterBottle.empty();
+    waterBottle.fill();
+    assertEquals(100, waterBottle.getVolume());
+  }
 }
